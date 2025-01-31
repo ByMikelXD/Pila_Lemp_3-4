d9bd03e005424d03a17101346397a7f36ab73dd3
ByMikelXD <146436293+ByMikelXD@users.noreply.github.com>
2025-01-31 16:22:50 +0100

    Update script-sql.sh

--- script-sql.sh
+++ script-sql.sh
@@ -1,3 +1,9 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install -y default-mysql-server
+sudo apt-get update && sudo apt-get install -y mariadb-server
+
+sudo sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo systemctl restart mariadb
+
+mysql -u root -e "CREATE DATABASE wordpress_db;"
+mysql -u root -e "CREATE USER 'wp_user'@'%' IDENTIFIED BY 'password';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wp_user'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
