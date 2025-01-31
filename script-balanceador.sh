b6bf2fa03404df6c0f46d7b140019e1b5a25e5c4
ByMikelXD <146436293+ByMikelXD@users.noreply.github.com>
2025-01-31 16:22:10 +0100

    Update script-balanceador.sh

--- script-balanceador.sh
+++ script-balanceador.sh
@@ -1,3 +1,25 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install -y nginx
+sudo apt update && sudo apt install -y nginx
+systemctl start nginx
+systemctl enable nginx
+
+cat <<EOL | sudo tee /etc/nginx/sites-enabled/balanceador
+upstream servidoresweb {
+    server IP_WEB1;
+    server IP_WEB2;
+}
+
+server {
+    listen 80;
+    server_name balanceador;
+
+    location / {
+        proxy_redirect off;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $http_host;
+        proxy_pass http://servidoresweb;
+    }
+}
+EOL
+
+systemctl restart nginx
